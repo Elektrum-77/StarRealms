3d7181a4a64b817b0131541d8d1e36feb8606720
BABILLON Brian <brian.babillon@gmail.com>
2020-04-14 16:33:04 +0200

    Update SimpleGameController.java

--- src/fr/dut/info/starRealms/SimpleGameController.java
+++ src/fr/dut/info/starRealms/SimpleGameController.java
@@ -24,143 +24,139 @@ import fr.umlv.zen5.Event.Action;
 
 public class SimpleGameController {
 
-	static void simpleGame(ApplicationContext context) {
-		while(true) { // boucle du programme
-			// Background Aléatoire
-			Image bg1 = new ImageIcon(SimpleGameController.class.getClassLoader().getResource("img/background/bg.jpg")).getImage();
-			Image bg2 = new ImageIcon(SimpleGameController.class.getClassLoader().getResource("img/background/bg2.jpg")).getImage();
-			Image bg3 = new ImageIcon(SimpleGameController.class.getClassLoader().getResource("img/background/bg4.jpg")).getImage();
-			Image bg4 = new ImageIcon(SimpleGameController.class.getClassLoader().getResource("img/background/bg6.jpg")).getImage();
-			Image bg5 = new ImageIcon(SimpleGameController.class.getClassLoader().getResource("img/background/bg7.jpg")).getImage();
-			Image bg6 = new ImageIcon(SimpleGameController.class.getClassLoader().getResource("img/background/bg8.jpg")).getImage();
-			Image bg7 = new ImageIcon(SimpleGameController.class.getClassLoader().getResource("img/background/bg9.jpg")).getImage();
-			
-			ArrayList<Image> bgList = new ArrayList<>();
-			bgList.add(bg1);
-			bgList.add(bg2);
-			bgList.add(bg3);
-			bgList.add(bg4);
-			bgList.add(bg5);
-			bgList.add(bg6);
-			bgList.add(bg7);
-			Random random = new Random();
-			int i = random.nextInt(bgList.size());
-			// Nouveau background defini aléatoirement
-			Image background = bgList.get(i);
-			// get the size of the screen
-			ScreenInfo screenInfo = context.getScreenInfo();
-			int width = (int) screenInfo.getWidth();
-			int height = (int) screenInfo.getHeight();
-			// Création du plateau
-			GameBoard gameBoard = new GameBoard();
-			// On ajoute les joueurs et les cartes
-			gameBoard = concreteCards(gameBoard);
-			// Mélanger tous chaque deck des joueurs
-			gameBoard.shuffleAllPlayersDeck();
-			// Mélanger le deck du jeu
-			gameBoard.getTradeDeck().shuffle();
-	
-			SimpleGameView view = new SimpleGameView(0, 10, width, height, background);
-			// On affiche la vue
-			view.draw(context, gameBoard);
-			// Nouveau point pour la location de la souris
-			Point2D.Float location;
-			
-			// On initialise le premier joueur en le désignant aléatoirement
-			int posPlayerInQueue = gameBoard.randomPlayer();
-			gameBoard.setThePlayer(posPlayerInQueue);
-			Player firstPlayer= gameBoard.getPlayingPlayer();
-			
-			// Tout le monde pioche 5 carte sauf le joueur qui commence pioche 3 cartes (voir règles)
-			gameBoard.everybodyDrawCardsExcept(firstPlayer,5);
-			firstPlayer.drawCards(3);
-			gameBoard.updateTradeRow();
-			
-			
-			view.draw(context, gameBoard);
-			while (true) { // boucle de la partie
-				Event event = context.pollOrWaitEvent(10); // modifier pour avoir un affichage fluide
-				if (event == null) { // no event
-					continue;
-				}
-				
-				Action action = event.getAction();
-				if (action == Action.KEY_PRESSED) {
-					
-					if(event.getKey().toString() == "SPACE") {
-						posPlayerInQueue+=1; // on passe le tour donc pos du joueur + 1
-						// même si la valeur envoyée est grande elle est traitée avec un modulo
-						gameBoard.setThePlayer(posPlayerInQueue); 
-						view.setCoordo(0,0);
-						
-					}else if(event.getKey().toString() == "Q") {
-						context.exit(0);
-						return;
-					}else if(event.getKey().toString() == "R") { // restart game
-						break;
-					}else if(event.getKey().toString() == "UP") { // cheat pool points
-						gameBoard.updateCombatPool(1);
-						gameBoard.updateTradePool(1);
-					}
-				}
-					
-				if(action == Action.POINTER_DOWN) {
-					location = event.getLocation();
-					view.setCoordo((int)location.x,(int)location.y);
-					
-					if(view.isEndOfTurn((int)location.x,(int)location.y)) {
-						posPlayerInQueue+=1; // on passe le tour donc pos du joueur + 1
-						gameBoard.endOfTurn(posPlayerInQueue); // même si la valeur envoyée est grande elle est traitée avec un modulo
-						view.draw(context, gameBoard);
-					}
-					
-				}else if(action == Action.POINTER_MOVE) {
-					location = event.getLocation();
-					if(view.setCoordoMiniMenu((int)location.x,(int)location.y)) {
-						view.draw(context, gameBoard);
-					}
-				}else if(action == Action.POINTER_UP) {
-					location = event.getLocation();
-					if(view.setCoordoPointerUp((int)location.x,(int)location.y)) {
-						view.draw(context, gameBoard);
-					}
+	static Image initBackgroundImage()
+	{
+		// Background Alï¿½atoire
+		Image bg1 = new ImageIcon(SimpleGameController.class.getClassLoader().getResource("img/background/bg.jpg")).getImage();
+		Image bg2 = new ImageIcon(SimpleGameController.class.getClassLoader().getResource("img/background/bg2.jpg")).getImage();
+		Image bg3 = new ImageIcon(SimpleGameController.class.getClassLoader().getResource("img/background/bg4.jpg")).getImage();
+		Image bg4 = new ImageIcon(SimpleGameController.class.getClassLoader().getResource("img/background/bg6.jpg")).getImage();
+		Image bg5 = new ImageIcon(SimpleGameController.class.getClassLoader().getResource("img/background/bg7.jpg")).getImage();
+		Image bg6 = new ImageIcon(SimpleGameController.class.getClassLoader().getResource("img/background/bg8.jpg")).getImage();
+		Image bg7 = new ImageIcon(SimpleGameController.class.getClassLoader().getResource("img/background/bg9.jpg")).getImage();
+		
+		ArrayList<Image> bgList = new ArrayList<>();
+		bgList.add(bg1);
+		bgList.add(bg2);
+		bgList.add(bg3);
+		bgList.add(bg4);
+		bgList.add(bg5);
+		bgList.add(bg6);
+		bgList.add(bg7);
+		Random random = new Random();
+		int i = random.nextInt(bgList.size());
 
+		return bgList.get(i);
+	}
+
+	static GameBoard initGameBoard() 
+	{
+		// Crï¿½ation du plateau
+		GameBoard gameBoard = new GameBoard();
+
+		// On ajoute les joueurs et les cartes
+		gameBoard = concreteCards(gameBoard);
+
+		// Mï¿½langer tous chaque deck des joueurs
+		gameBoard.shuffleAllPlayersDeck();
+
+		// Mï¿½langer le deck du jeu
+		gameBoard.getTradeDeck().shuffle();
+
+		// On initialise le premier joueur en le dï¿½signant alï¿½atoirement
+		int posPlayerInQueue = gameBoard.randomPlayer();
+		gameBoard.setThePlayer(posPlayerInQueue);
+		Player firstPlayer= gameBoard.getPlayingPlayer();
+		
+		// Tout le monde pioche 5 carte sauf le joueur qui commence pioche 3 cartes (voir rï¿½gles)
+		gameBoard.everybodyDrawCardsExcept(firstPlayer,5);
+		firstPlayer.drawCards(3);
+		gameBoard.updateTradeRow();
+
+		return gameBoard;
+	}
+
+
+	static void simpleGame(ApplicationContext context)
+	{
+
+		// Nouveau background defini alï¿½atoirement
+		Image background = initBackgroundImage();
+
+		// get the size of the screen
+		ScreenInfo screenInfo = context.getScreenInfo();
+		int width = (int) screenInfo.getWidth();
+		int height = (int) screenInfo.getHeight();
+
+		GameBoard gameBoard = initGameBoard();
+		
+		SimpleGameView view = new SimpleGameView(0, 10, width, height, background);
+		
+		// On affiche la vue
+		view.draw(context, gameBoard);
+		
+		// Nouveau point pour la location de la souris
+		Point2D.Float location;
+
+		while (true) { // boucle de la partie
+			Event event = context.pollOrWaitEvent(10); // modifier pour avoir un affichage fluide
+			if (event == null) { // no event
+				continue;
+			}
+			
+			Action action = event.getAction();
+			if (action == Action.KEY_PRESSED) {
+				
+				if(event.getKey().toString() == "SPACE") {
+					posPlayerInQueue+=1; // on passe le tour donc pos du joueur + 1
+					// mï¿½me si la valeur envoyï¿½e est grande elle est traitï¿½e avec un modulo
+					gameBoard.setThePlayer(posPlayerInQueue); 
+					view.setCoordo(0,0);
 					
-				}else {
-					view.setCoordoMiniMenu((int) 0, (int) 0);
+				}else if(event.getKey().toString() == "Q") {
+					context.exit(0);
+					return;
+				}else if(event.getKey().toString() == "R") { // restart game
+					break;
+				}else if(event.getKey().toString() == "UP") { // cheat pool points
+					gameBoard.updateCombatPool(1);
+					gameBoard.updateTradePool(1);
 				}
-	
-				if(!(action == Action.POINTER_MOVE)) {
+			}
+				
+			if(action == Action.POINTER_DOWN) {
+				location = event.getLocation();
+				view.setCoordo((int)location.x,(int)location.y);
+				
+				if(view.isEndOfTurn((int)location.x,(int)location.y)) {
+					posPlayerInQueue+=1; // on passe le tour donc pos du joueur + 1
+					gameBoard.endOfTurn(posPlayerInQueue); // mï¿½me si la valeur envoyï¿½e est grande elle est traitï¿½e avec un modulo
 					view.draw(context, gameBoard);
 				}
 				
-				if(gameBoard.isEndOfGame()) {
-					break;
+			}else if(action == Action.POINTER_MOVE) {
+				location = event.getLocation();
+				if(view.setCoordoMiniMenu((int)location.x,(int)location.y)) {
+					view.draw(context, gameBoard);
 				}
+			}else if(action == Action.POINTER_UP) {
+				location = event.getLocation();
+				if(view.setCoordoPointerUp((int)location.x,(int)location.y)) {
+					view.draw(context, gameBoard);
+				}
+
 				
+			}else {
+				view.setCoordoMiniMenu((int) 0, (int) 0);
+			}
+
+			if(!(action == Action.POINTER_MOVE)) {
+				view.draw(context, gameBoard);
 			}
 			
-			// quitter le jeu ou recommencer
-			while (true) {
-				Event event = context.pollOrWaitEvent(10); 
-				if (event == null) { // no event
-					continue;
-				}
-				
-				Action action = event.getAction();
-				if (action == Action.KEY_PRESSED) {
-					if(event.getKey().toString() == "Q") {
-						context.exit(0);
-						return;
-					}else if(event.getKey().toString() == "R") {
-						break;
-					}
-				}
-				
+			if(gameBoard.isEndOfGame()) {
+				break;
 			}
-			
-			
-			
 		}
 	}
 
@@ -171,7 +167,7 @@ public class SimpleGameController {
 	}
 	
 	public static GameBoard concreteCards(GameBoard gameBoard) {
-		// Création des cartes:
+		// Crï¿½ation des cartes:
 		
 		/* BLOB */
 		Ship battleBlob = new Ship("-1", new ImageIcon(SimpleGameController.class.getClassLoader().getResource("img/Core-set/Battle-blob.jpg")).getImage(),"Battle Blob","Blob",6,new Primary(8,0,0,0), new Ally(0,0,0,1), new Scrap(4,0,0,0));
@@ -197,9 +193,9 @@ public class SimpleGameController {
 		cutter.addDescription("Built for cargo,\narmed for conflict\nVersatility for an"
 				+ "\nunpredictable unvierse.\nPremier Aerospace Cargo\nEnterprises.\n");
 
-		// Création d'un primary avec action spéciale
+		// Crï¿½ation d'un primary avec action spï¿½ciale
 		Primary embassyYachtPrimary = new Primary(0,2,3,0);
-		embassyYachtPrimary.addSpecialAction(new SpeActEmbassyYacht()); // ajout d'une action spéciale au primary
+		embassyYachtPrimary.addSpecialAction(new SpeActEmbassyYacht()); // ajout d'une action spï¿½ciale au primary
 		
 		Ship embassyYacht = new Ship("-1", new ImageIcon(SimpleGameController.class.getClassLoader().getResource("img/Core-set/Embassy-Yacht.jpg")).getImage(),"Embassy Yacht","Trade Federation", 3, embassyYachtPrimary);
 		cutter.addDescription("War should always \nbe a last resort,\nit's bad for the bottom line.\n");
@@ -220,7 +216,7 @@ public class SimpleGameController {
 		gameBoard.addPlayer(new Player("Joueur 2"));
 
 
-		// Création de la pile Explorers
+		// Crï¿½ation de la pile Explorers
 		gameBoard.initialiseExplorers(explorer, 10);
 
 		// Initialisation des decks joueurs
